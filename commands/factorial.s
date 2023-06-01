1f663c90160d6ac39ed2c109defdb765355d63ba
Zisis Vakras <zisis.vakras@protonmail.com>
2023-06-02 00:13:06 +0300

    factorial

---
+++ commands/factorial.s
@@ -0,0 +1,18 @@
+#  Returns $a0! in $v0
+factorial:
+    addi $sp, $sp, -4    # make space in stack
+    sw $t0, 0($sp)
+    move $v0, $a0
+    bne $v0, $zero, factorialloop
+    li $v0, 1            # case of 0!
+    j factorialexit
+    factorialloop:
+    addi $t0, $v0, -1
+    beq $t0, $zero, factorialexit
+    mult $v0, $t0
+    mflo $v0
+    j factorialloop
+    factorialexit:
+    lw $t0, 0($sp)
+    addi $sp, $sp, 4
+    jr $ra
