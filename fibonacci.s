554fdf10395633e877af0c23d28e6ae3d4e91bad
Zisis Vakras <zisis.vakras@protonmail.com>
2023-06-01 22:15:21 +0300

    better comments

--- fibonacci.s
+++ fibonacci.s
@@ -16,23 +16,23 @@ main:
     syscall
 
 fib:
-    slti $t0, $a0, 2 # if (n < 2) return n;
+    slti $t0, $a0, 2    # if (n < 2) return n;
     beq $t0, $zero, fibrec
-    move $v0, $a0 # return result
+    move $v0, $a0       # return result
     jr $ra
 fibrec:
-    addi $sp, $sp, -8 # make space in stack
-    sw $ra, 4($sp) # keep every ra and current n
+    addi $sp, $sp, -8   # make space in stack
+    sw $ra, 4($sp)      # keep ra and current n
     sw $a0, 0($sp)
-    addi $a0, $a0, -1 # fib(n - 1)
+    addi $a0, $a0, -1   # fib(n - 1)
     jal fib
     lw $a0, 0($sp) 
-    addi $a0, $a0, -2 # fib(n - 2)
-    sw $v0, 0($sp) # save result of fib(n - 1) in the
-                   # useless now space $a0 had
+    addi $a0, $a0, -2   # fib(n - 2)
+    sw $v0, 0($sp)      # save result of fib(n - 1) in the
+                        # useless now space $a0 had
     jal fib
     lw $t0, 0($sp)
-    add $v0, $t0, $v0 # add both results and return
+    add $v0, $t0, $v0   # add both results and return
     lw $ra, 4($sp)
     addi $sp, $sp, 8
     jr $ra
