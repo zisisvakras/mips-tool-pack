50cd86172e62dee74674467d0f0139df7602bd98
Zisis Vakras <zisis.vakras@protonmail.com>
2023-06-01 22:49:00 +0300

    label fix

--- commands/strcmp.s
+++ commands/strcmp.s
@@ -37,23 +37,23 @@ strneq:
     sw $t1, 4($sp)
     sw $t2, 0($sp)
     li $t2, 0            # load 0 in t2 (it will be our counter)
-    streqloop:
+    strneqloop:
     lb $t0, $t2($a0)
     lb $t1, $t2($a1)
-    bne $t0, $t1, streqexitno
-    beq $t0, $zero, streqexityes
+    bne $t0, $t1, strneqexitno
+    beq $t0, $zero, strneqexityes
     addi $t2, $t2, 1
-    j streqloop
+    j strneqloop
 
-    streqexitno:
+    strneqexitno:
     li $v0, 1
-    j streqexit
+    j strneqexit
 
-    streqexityes:
+    strneqexityes:
     li $v0, 0
-    j streqexit
+    j strneqexit
 
-    streqexit:
+    strneqexit:
     lw $t0, 8($sp)       # restore t0, t1, t2 from stack
     lw $t1, 4($sp)
     lw $t2, 0($sp)
