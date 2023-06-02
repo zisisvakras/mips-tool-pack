e94a2f9bb41a1bf1d83e1c249a9ea4d0d15f507c
Zyro <62178614+yannisxiros@users.noreply.github.com>
2023-06-02 13:57:37 +0300

    Update strlen.s

--- functions/strlen.s
+++ functions/strlen.s
@@ -1,19 +1,11 @@
 #  Calculate the length of string in $a0
 #  Returns length in $v0 (without \0)
+#  overwrites $t0
 strlen:
-    addi $sp, $sp, -8    # make space in stack
-    sw $t0, 4($sp)       # keep t0, t1 in stack
-    sw $t1, 0($sp)
-    li $t1, 0            # load 0 in t1 (it will be our counter)
-    strlenloop:
-    lb $t0, $t1($a0)
-    beq $t0, $zero, strlenexit
-    addi $t1, $t1, 1
-    j strlenloop
-
-    strlenexit:
-    move $v0, $t1
-    lw $t0, 4($sp)       # restore t0, t1, t2 from stack
-    lw $t1, 0($sp)
-    addi $sp, $sp, 8
+    addi $v0, $a0, -1
+strlenloop:
+    lb $t0, 1($v0)
+    addi $v0, $v0, 1
+    bne $t0, $zero, strlenloop
+    sub $v0, $v0, $a0
     jr $ra
