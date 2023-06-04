ac566340d035efc9e90a6a5b72843370882d6300
Zisis Vakras <zisis.vakras@protonmail.com>
2023-06-04 19:21:40 +0300

    strlen,strcmp fixed?

--- functions/strlen.s
+++ functions/strlen.s
@@ -1,11 +1,14 @@
 #  Calculate the length of string in $a0
 #  Returns length in $v0 (without \0)
-#  overwrites $t0
 strlen:
+    addi $sp, $sp, -4    # make space in stack
+    sw $t0, 4($sp)       # keep t0 in stack
     addi $v0, $a0, -1
 strlenloop:
     lb $t0, 1($v0)
     addi $v0, $v0, 1
     bne $t0, $zero, strlenloop
-    sub $v0, $v0, $a0
+    sub $v0, $v0, $a0    # prepare return value
+    lw $t0, 4($sp)       # restore t0 from stack
+    addi $sp, $sp, 4
     jr $ra
