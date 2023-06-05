1d726d8455590900f3047f43311d55d74d52b774
Zisis Vakras <79864046+zisisvakras@users.noreply.github.com>
2023-06-05 15:09:20 +0300

    Update strlen.s (#14)

--- functions/strlen.s
+++ functions/strlen.s
@@ -2,13 +2,13 @@
 #  Returns length in $v0 (without \0)
 strlen:
     addi $sp, $sp, -4    # make space in stack
-    sw $t0, 4($sp)       # keep t0 in stack
+    sw $t0, 0($sp)       # keep t0 in stack
     addi $v0, $a0, -1
 strlenloop:
     lb $t0, 1($v0)
     addi $v0, $v0, 1
     bne $t0, $zero, strlenloop
     sub $v0, $v0, $a0    # prepare return value
-    lw $t0, 4($sp)       # restore t0 from stack
+    lw $t0, 0($sp)       # restore t0 from stack
     addi $sp, $sp, 4
     jr $ra
