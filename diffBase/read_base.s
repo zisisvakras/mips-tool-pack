58b4739d0a810829908004c31ac03318882e5989
Zyro <62178614+yannisxiros@users.noreply.github.com>
2023-06-07 22:47:42 +0300

    print and read (stef) (#16)

--- diffBase/read_base.s
+++ diffBase/read_base.s
@@ -77,3 +77,76 @@ read_bin_val:
     sll $s0, $s0, 1
     or $s0, $s0, $v0
     j read_bin_loop
+
+# $a0: number base (2..16)
+# $v0: output
+# Characters should be in capital, stops at the first non complian char
+read_num:
+    addi $sp, $sp, -12
+    sw  $ra, 0($sp)
+    sw  $s0, 4($sp)
+    sw  $s1, 8($sp)
+    add $s0, $zero, $zero
+    add $s1, $zero, $a0
+read_num_loop:
+    li $v0, 12
+    syscall
+    addi $v0, $v0, -48
+    bltz $v0, read_num_end
+    slti $a0, $v0, 10
+    bne $a0, $zero, read_num_check
+    addi $v0, $v0, -17
+    bltz $v0, read_num_end
+    addi $v0, $v0, 10
+    slt $a0, $v0, $s1
+read_num_check:
+    bne $a0, $zero, read_num_val
+read_num_end:
+    add $v0, $s0, $zero
+    add $a0, $s1, $zero
+    lw  $s1, 8($sp)
+    lw  $s0, 4($sp)
+    lw  $ra, 0($sp)
+    addi $sp, $sp, 12
+    jr $ra
+read_num_val:
+    mul $s0, $s0, $s1
+    or $s0, $s0, $v0
+    j read_num_loop
+
+# $a0: number base (2..16), $a1: the string
+# $v0: output
+# Characters should be in capital, stops at the first non complian char
+str_num:
+    addi $sp, $sp, -16
+    sw  $ra, 0($sp)
+    sw  $s0, 4($sp)
+    sw  $s1, 8($sp)
+    sw  $a1, 12($sp)
+    add $s0, $zero, $zero
+    add $s1, $zero, $a0
+str_num_loop:
+    lb $v0, 0($a1)
+    addi $a1, $a1, 1
+    addi $v0, $v0, -48
+    bltz $v0, str_num_end
+    slti $a0, $v0, 10
+    bne $a0, $zero, str_num_check
+    addi $v0, $v0, -17
+    bltz $v0, str_num_end
+    addi $v0, $v0, 10
+    slt $a0, $v0, $s1
+str_num_check:
+    bne $a0, $zero, str_num_val
+str_num_end:
+    add $v0, $s0, $zero
+    lw  $a1, 12($sp)
+    lw  $s1, 8($sp)
+    lw  $s0, 4($sp)
+    lw  $ra, 0($sp)
+    addi $sp, $sp, 16
+    jr $ra
+str_num_val:
+    mul $s0, $s0, $s1
+    or $s0, $s0, $v0
+    j str_num_loop
