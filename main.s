88ca4201602ecc397006c49e84f8a626a5f3fb1a
Alkis <alkishon@gmail.com>
2023-06-10 21:20:45 +0300

    undo changes in main

--- main.s
+++ main.s
@@ -1,10 +1,25 @@
 #  MESSAGES
 .data
-    ReadInt: .asciiz "Insert a number:"
-    ReadString: .asciiz "Insert a string :"
-    equal: .asciiz " = "
-    msg_is: .asciiz " is: "
-    Bye: .asciiz "\nEnd of Program...\n"
+    ReadInt: .asciiz "Insert an integer :"
+    ReadFloating: .asciiz "Insert a float :"
+    ReadDoubleNum: .asciiz "Insert a double :"
+    ReadStringCharacters: .asciiz "Insert a string :"
+    Result: .asciiz "The result is :"
+    Final: .asciiz "The final <type> is: "
+    Difference: .asciiz "The difference is :"
+    Quotient: .asciiz "The Quotient is :"
+    Remainder: .asciiz "The Remainder is :"
+    Factorial: .asciiz "The factorial of the number "
+    General_Start: .asciiz "The "
+    Power: .asciiz " th power of the number "
+    General_End: .asciiz " is: "
+    StringMessage: .asciiz "The final string is: "
+    Converted1: .asciiz "The converted float is: "
+    Converted2: .asciiz " from: "
+    ProgramEnd: .asciiz "\nEND OF PROGRAM\n"
+    ErrorMessages: .asciiz "The character is not a number\n"
+    EqualMessage: .asciiz "The strings are equal"
+    NotEqualMessage: .asciiz "The strings are not equal"
     wronginput: .asciiz "Wrong Input!"
     endl: .asciiz "\n"
 
@@ -13,77 +28,84 @@
 
 main:
     j exit
-## FUNCTIONS ## (Call with: jal function_name)
 
-exit:                       # Exits the program 
-    li $v0, 10
-    syscall
-#  
 
-read_int:                   #  Read integer from input 
-    li $v0, 5               #  Integer will be in $v0 
-    syscall
-    jr $ra
+###
+#  A collection of syscalls in mips-assembly
+### 
 
-print_int:                  #  Prints integer given in $a0 
+#  Prints integer given in $a0
+print_int:
     li $v0, 1
     syscall
     jr $ra
 
-#  
-
-read_float:                 #  Read float from input 
-    li $v0, 6               #  Float will be in $f0 
+#  Prints float given in $f12
+#  Use "mov.s $f12, $???" to move float to reg
+print_float:
+    li $v0, 2
     syscall
     jr $ra
 
-print_float:                #  Prints float given in $f12                
-    li $v0, 2               #  Use "mov.s $f12, $???" to move float to reg 
-    syscall
-    jr $ra
-#  
-
-read_double:                #  Read double from input 
-    li $v0, 7               #  Double will be in $f0/$f1
-    syscall                 #  !NOTE: that double uses 2 registers (pair of even-odd)
-    jr $ra
-
-print_double:               #  Prints double given in $f12/$f13
-    li $v0, 3               #  Use "mov.d $f12, $???" to move double to reg
-    syscall                 #  !NOTE: that double uses 2 registers (pair of even-odd) 
-    jr $ra
-#
-
-read_string:                #  Read string from input 
-    li $v0, 8               #  $a0 will be buffer address and $a1 the length (including the space made for \0) 
-    syscall                 #  Remember that u can allocate space for a buffer in .data with .space (len)
-    jr $ra
-#
-
-print_string:               #  Prints string given in $a0 
-    li $v0, 4               #  Use "la $a0, label" to specify string address
-    syscall
-    jr $ra
-#
-
-read_char:                  #  Read char from input 
-    li $v0, 12              #  Char will be in $v0 
+#  Prints double given in $f12
+#  Use "mov.d $f12, $???" to move double to reg
+#  Remember that double uses 2 registers (pair of even-odd)
+print_double:
+    li $v0, 3
     syscall
     jr $ra
 
-print_char:                 #  Prints char given in $a0 
+#  Prints string given in $a0
+#  Use "la $a0, label" to specify string address
+print_string:
+    li $v0, 4
+    syscall
+    jr $ra
+
+#  Read integer from input
+#  Integer will be in $v0
+read_int:
+    li $v0, 5
+    syscall
+    jr $ra
+
+#  Read float from input
+#  Float will be in $f0
+read_float:
+    li $v0, 6
+    syscall
+    jr $ra
+
+#  Read double from input
+#  Double will be in $f0 (and $f1)
+#  Remember that double uses 2 registers (pair of even-odd)
+read_double:
+    li $v0, 7
+    syscall
+    jr $ra
+
+#  Read string from input
+#  $a0 will be buffer address and $a1 the length (including the space made for \0)
+#  Remember that u can allocate space for a buffer in .data with .space (len)
+read_string:
+    li $v0, 8
+    syscall
+    jr $ra
+
+# Exits the program
+exit:
+    li $v0, 10
+    syscall
+
+#  Prints char given in $a0
+print_char:
     li $v0, 11
     syscall
     jr $ra
-#
 
-print_endl:                 # prints '\n'
-    addi $sp, $sp, -4 
-    sw $a0, 0($sp)
-    li $a0, 10
-    li $v0, 11
+#  Read char from input
+#  Char will be in $v0
+read_char:
+    li $v0, 12
     syscall
-    lw $a0, 0($sp)
-    addi $sp, $sp, 4
     jr $ra
-#
