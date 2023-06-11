9e533f45de25144ba485a15cfbce51a9475d08bd
thanostzia <thanostzia@gmail.com>
2023-06-11 18:51:58 +0300

    final touches

--- labs/lab4/lab_4_2.b.s
+++ labs/lab4/lab_4_2.b.s
@@ -4,49 +4,28 @@
 .globl __start
 __start:
     # start of main program
-    la $a0,prompt
-    li $v0,4
-    syscall # display "Enter integer number :"
+    la $a0, prompt
+    li $v0, 4
+    syscall # display "Enter integer number: "
 
-    li $v0,5
+    li $v0, 5
     syscall # read integer
 
     move $a0, $v0       # move the integer that was given from the $v0 to $a0
     jal proc        # jump to procedure
 
-    move $t0,$v0        # move the result of the procedure from $v0 to $t0
+    move $t0, $v0        # move the result of the procedure from $v0 to $t0
 
-    la $a0,endl
-    li $v0,4
+    la $a0, endl
+    li $v0, 4
     syscall             # display end of line
-    move $a0,$t0
+    move $a0, $t0
 
-    li $v0,10
+    li $v0, 10
     syscall             # exit
+
 # end of main program
 # start of procedure
-#
-# proc: # Arguement: $a0 - integer number
-#         li $v0, 1
-#         syscall
-
-#         ble $a0, $zero, next    # if $a0 <= 0, stop the procedure
-#                                 # note: < is for case of $a0 being negative
-
-#         addi $sp, $sp, -4       # store the value of $a0 in stack
-#         sw $a0, 0($sp)
-
-#         la $a0,endl
-#         li $v0,4
-#         syscall                 # display end of line
-
-#         lw $a0, 0($sp)          # retrieve the old value of $a0 from stack
-#         addi $sp, $sp, 4
-
-#         addi $a0, $a0, -1       # decrease the value of $a0 by 1
-#         move $v0, $a0           # move the new decreased number to $v0, which holds the result of a procedure   
-#         j proc                  # jump to procedure, so it is recursive
-# end of procedure
 
 proc: # Arguement: $a0 - integer number
         addi $sp, $sp, -8       # store the value of $ra in stack
@@ -66,8 +45,8 @@ proc: # Arguement: $a0 - integer number
 
 proc_rec:
         move $t0, $a0           # move the value of $a0 to $t0
-        la $a0,endl
-        li $v0,4
+        la $a0, endl
+        li $v0, 4
         syscall                 # display end of line
         move $a0, $t0
 
@@ -83,13 +62,3 @@ proc_rec:
 .data
 prompt: .asciiz "Enter integer number:\n"
 endl: .asciiz "\n"
-
-
-
-# void printing_a0(int a0)
-# {
-#     printf("%d\n", a0);
-#     if (a0 <= 0)
-#         return;
-#     printing_a0(--a0);
-# }
