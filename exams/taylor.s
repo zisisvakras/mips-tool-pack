7ac6ce2ff62836c25b051bc81ec320b8dd7b5967
LAPTOP-IH0FE3SM\xiros <yannis.xiros@gmail.com>
2023-09-19 16:04:46 +0300

    refactoring taylor

--- exams/taylor.s
+++ exams/taylor.s
@@ -1,30 +1,10 @@
-#possible exam problem
-#calculates taylor series: x^n/n! 
-#of the function: e^x 
+# possible exam problem
+# calculates Τaylor series: x^n/n! 
+# of the function: e^x 
 
 #  MESSAGES
 .data
-    ReadInt: .asciiz "Insert an integer :"
     ReadFloating: .asciiz "Insert a float :"
-    ReadDoubleNum: .asciiz "Insert a double :"
-    ReadStringCharacters: .asciiz "Insert a string :"
-    Result: .asciiz "The result is :"
-    Final: .asciiz "The final <type> is: "
-    Difference: .asciiz "The difference is :"
-    Quotient: .asciiz "The Quotient is :"
-    Remainder: .asciiz "The Remainder is :"
-    Factorial: .asciiz "The factorial of the number "
-    General_Start: .asciiz "The "
-    Power: .asciiz " th power of the number "
-    General_End: .asciiz " is: "
-    StringMessage: .asciiz "The final string is: "
-    Converted1: .asciiz "The converted float is: "
-    Converted2: .asciiz " from: "
-    ProgramEnd: .asciiz "\nEND OF PROGRAM\n"
-    ErrorMessages: .asciiz "The character is not a number\n"
-    EqualMessage: .asciiz "The strings are equal"
-    NotEqualMessage: .asciiz "The strings are not equal"
-    wronginput: .asciiz "Wrong Input!"
 
 .text
 .globl main
@@ -75,28 +55,16 @@ done:
     add.s $f12, $f3, $f7 
     jal print_float
     j exit
+
+
 ###
 #  A collection of syscalls in mips-assembly
 ### 
-
-#  Prints integer given in $a0
-print_int:
-    li $v0, 1
-    syscall
-    jr $ra
 
 #  Prints float given in $f12
 #  Use "mov.s $f12, $???" to move float to reg
 print_float:
     li $v0, 2
-    syscall
-    jr $ra
-
-#  Prints double given in $f12
-#  Use "mov.d $f12, $???" to move double to reg
-#  Remember that double uses 2 registers (pair of even-odd)
-print_double:
-    li $v0, 3
     syscall
     jr $ra
 
@@ -107,25 +75,10 @@ print_string:
     syscall
     jr $ra
 
-#  Read integer from input
-#  Integer will be in $v0
-read_int:
-    li $v0, 5
-    syscall
-    jr $ra
-
 #  Read float from input
 #  Float will be in $f0
 read_float:
     li $v0, 6
-    syscall
-    jr $ra
-
-#  Read double from input
-#  Double will be in $f0 (and $f1)
-#  Remember that double uses 2 registers (pair of even-odd)
-read_double:
-    li $v0, 7
     syscall
     jr $ra
 
@@ -141,19 +94,6 @@ read_string:
 exit:
     li $v0, 10
     syscall
-
-#  Prints char given in $a0
-print_char:
-    li $v0, 11
-    syscall
-    jr $ra
-
-#  Read char from input
-#  Char will be in $v0
-read_char:
-    li $v0, 12
-    syscall
-    jr $ra
 
 #  Prints new line, overwrites $a0
 print_endl:
