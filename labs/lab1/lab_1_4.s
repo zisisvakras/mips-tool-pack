5143ee3b3dd299db6e76a15a81ff3654dcecc9ff
StathisKons <konstantinou.stathis@gmail.com>
2024-05-11 23:23:43 +0300

    fix lab1.4

--- labs/lab1/lab_1_4.s
+++ labs/lab1/lab_1_4.s
@@ -1,39 +1,35 @@
-# Read String and Decrease its Length to 5
-
 .text
-.globl __start
 
-__start:
+.globl main
 
-    li $v0, 8           # read string
-    la $a0, buffer      # we load the address of label "buffer" in $a0,
-    syscall             # so the string that is read is stored there
+main:
 
-    la $a0, buffer      # we load the address of label "buffer" in $a0, so now the string can be printed
-    li $v0, 4           # print string
-    syscall
+li $v0, 8	# read string
+li $a1, 20	# specify length to read
+la $a0, buffer	# store read string in buffer
+syscall		
 
-    la $a0, endl        # we load the address of "endl" label in $a0, so the new line character "\n" is printed
-    li $v0, 4           # print string "\n"
-    syscall
+li $v0, 4	# print string
+la $a0, newl	# print new line
+syscall
 
-    addi $t0, $zero, 0  # we store the value 0 in $t0
-    sb $t0, buffer + 5  # store byte qual to 0 in address of label "buffer" + 5
-                        # so the string is changed and the "\n" character is in 6th place
-                        # tricking MIPS its length decreased
+ulh $t0, newl	# unalligned since its not a multiple of 4
+ush $t0, buffer + 5	# add new line characther and null byte after 5th char
+			# to only print the first 5 characthers
+			# (MIPS prints whats in memory until it reaches
+			# a NULL byte (\0) so by placing one we tell it stop printing)
 
-    la $a0, buffer      # we load the address of "buffer" in $a0, so the new string is printed
-    li $v0, 4           # print string
-    syscall
+la $a0, buffer	# print buffer	
+# no need to li $a0, 4 , its already there
+syscall		# print string
 
-    la $a0, endl        # we load the address of "endl" in $a0, so the new line character "\n" is printed
-    li $v0, 4           # print string "\n"
-    syscall
+li $v0, 10	# exit
+syscall
 
-    li $v0, 10          # exit
-    syscall
+
+
+
 
 .data
-
-buffer: .space 21
-endl: .asciiz "\n"
+buffer: .space 21	# pilot said to read up to 20 chars
+newl: .asciiz "\n"	# asciiz adds a NULL byte after the string
