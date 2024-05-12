e93b17f4bcee1681eb0800d0495d377a4b61edb6
StathisKons <konstantinou.stathis@gmail.com>
2024-05-12 20:33:56 +0300

    fix lab1.4

--- labs/lab1/lab_1_4.s
+++ labs/lab1/lab_1_4.s
@@ -1,35 +1,36 @@
+# Read String and Decrease its Length to 5
+
+.data
+buffer: .space 21	# pilot said to read up to 20 chars (extra space for null byte)
+newl: .asciiz "\n"	# asciiz adds a NULL byte after the string
+
 .text
 
 .globl main
 
 main:
 
-li $v0, 8	# read string
-li $a1, 20	# specify length to read
+li $v0, 8		# read string
+li $a1, 21		# specify length to read
 la $a0, buffer	# store read string in buffer
 syscall		
 
-li $v0, 4	# print string
 la $a0, newl	# print new line
+li $v0, 4		# print string
 syscall
 
-ulh $t0, newl	# unalligned since its not a multiple of 4
-ush $t0, buffer + 5	# add new line characther and null byte after 5th char
-			# to only print the first 5 characthers
-			# (MIPS prints whats in memory until it reaches
-			# a NULL byte (\0) so by placing one we tell it stop printing)
+sb $zero, buffer + 5	# add null byte after 5th char
+						# to only print the first 5 characthers
+						# (MIPS prints whats in memory until it reaches
+						# a NULL byte (\0) so by placing one we tell it stop)
 
 la $a0, buffer	# print buffer	
-# no need to li $a0, 4 , its already there
-syscall		# print string
+li $v0, 4		# didnt actually need this, v0 is already 4
+syscall			# print string
+
+la $a0, newl	# print new line
+li $v0, 4		# print string
+syscall
 
 li $v0, 10	# exit
 syscall
-
-
-
-
-
-.data
-buffer: .space 21	# pilot said to read up to 20 chars
-newl: .asciiz "\n"	# asciiz adds a NULL byte after the string
