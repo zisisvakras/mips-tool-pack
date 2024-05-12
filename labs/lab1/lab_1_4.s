c9c77f4b01c0ea35f931f9accddf405dca4be615
Στάθης Κωνσταντίνου <87396802+StathisKons@users.noreply.github.com>
2024-05-12 22:56:15 +0300

    fix lab1.4 (#28)
    * fix lab1.4
    
    * fix lab1.4
    
    * allign comments
    
    * print read string
    
    * allign comments fr fr :skull:

--- labs/lab1/lab_1_4.s
+++ labs/lab1/lab_1_4.s
@@ -1,39 +1,40 @@
 # Read String and Decrease its Length to 5
 
-.text
-.globl __start
-
-__start:
-
-    li $v0, 8           # read string
-    la $a0, buffer      # we load the address of label "buffer" in $a0,
-    syscall             # so the string that is read is stored there
-
-    la $a0, buffer      # we load the address of label "buffer" in $a0, so now the string can be printed
-    li $v0, 4           # print string
-    syscall
-
-    la $a0, endl        # we load the address of "endl" label in $a0, so the new line character "\n" is printed
-    li $v0, 4           # print string "\n"
-    syscall
-
-    addi $t0, $zero, 0  # we store the value 0 in $t0
-    sb $t0, buffer + 5  # store byte qual to 0 in address of label "buffer" + 5
-                        # so the string is changed and the "\n" character is in 6th place
-                        # tricking MIPS its length decreased
-
-    la $a0, buffer      # we load the address of "buffer" in $a0, so the new string is printed
-    li $v0, 4           # print string
-    syscall
-
-    la $a0, endl        # we load the address of "endl" in $a0, so the new line character "\n" is printed
-    li $v0, 4           # print string "\n"
-    syscall
-
-    li $v0, 10          # exit
-    syscall
-
 .data
+buffer: .space 21   # pilot said to read up to 20 chars (extra space for null byte)
+newl: .asciiz "\n"  # asciiz adds a NULL byte after the string
 
-buffer: .space 21
-endl: .asciiz "\n"
+.text
+
+.globl main
+
+main:
+
+li $v0, 8       # read string
+li $a1, 21      # specify length to read
+la $a0, buffer  # store read string in buffer
+syscall		
+
+la $a0, buffer  # not needed, a0 already holds the address of buffer
+li $v0, 4       # print string
+syscall
+
+la $a0, newl    # print new line
+li $v0, 4       # print string, not necessary, v0 is already 4
+syscall
+
+sb $zero, buffer + 5    # add null byte after 5th char
+			            # to only print the first 5 characthers
+			            # (MIPS prints whats in memory until it reaches
+			            # a NULL byte (\0) so by placing one we tell it stop)
+
+la $a0, buffer  # print buffer	
+li $v0, 4       # didnt actually need this, v0 is already 4
+syscall         # print string
+
+la $a0, newl	# print new line
+li $v0, 4	    # print string
+syscall
+
+li $v0, 10	# exit
+syscall
