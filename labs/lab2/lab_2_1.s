b38bdb9f28337a688025da4c6fdae26bb4346a69
Zisis Vakras <zisis.vakras@protonmail.com>
2024-05-13 20:56:28 +0300

    minor comment change

--- labs/lab2/lab_2_1.s
+++ labs/lab2/lab_2_1.s
@@ -1,19 +1,16 @@
-# encode a 32bit number using Grey encoding
+# Εncode a 32bit number using Grey encoding
 
-# insert value at s0, manually from qtspim window, before running the program
-# result is stored at s1
+# insert value at s0, manually from qtspim window,
+# before running the program result is stored at s1
 
 .text
-
 .globl main
 
 main:
-srl $t0, $s0, 1     # shift right to prepare for xor, logical shift 
-                    # to make most significant bit of t0 0 so that
-                    # after xor most significant bit of s1 is equal
-                    # to most significant bit of s0
-xor $s1, $s0, $t0
-
-
-li $v0, 10          # exit
-syscall
+    # Obtain the g_i bit sequence
+    srl $t0, $s0, 1
+    # Perform the encoding
+    xor $s1, $s0, $t0
+    # Exit
+    li $v0, 10
+    syscall
