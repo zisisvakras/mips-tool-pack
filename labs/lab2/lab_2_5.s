94d8eadd2360fe0b04dfb43dbef5d8f4945f0921
George Siafarikas <52412315+siafgeorge@users.noreply.github.com>
2024-05-15 21:09:55 +0300

    Using less instructions
    Always the least instructions is better.

--- labs/lab2/lab_2_5.s
+++ labs/lab2/lab_2_5.s
@@ -8,16 +8,13 @@ main:
     # load
     lw   $s0, w0
     lw   $s1, w1
-    addu $s2, $s1, $s0 # sum
-    # si'ai + si'bi
-    nor  $s2, $s2, $zero
-    or   $s3, $s1, $s0
-    and  $s3, $s3, $s2
-    # + aibi
-    and  $t0, $s1, $s0
-    or   $s3, $s3, $t0
-    # shift to leave 1 bit
-    srl  $s3, $s3, 31
+
+    addu $s0, $t1, $t2
+    xor $s1, $s0, $t1
+    xor $s2, $s0, $t2
+
+    or $s3, $s1, $s2
+    srl $s3, $s3, 31
     # exit
     li   $v0, 10
     syscall
