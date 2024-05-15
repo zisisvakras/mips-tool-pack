924f3121917391222af74ee1d2230c3bf87863a3
TR1LON <d.m.alexandris@gmail.com>
2024-05-15 22:05:19 +0300

    Update lab_2_5.s

--- labs/lab2/lab_2_5.s
+++ labs/lab2/lab_2_5.s
@@ -1,18 +1,23 @@
 .data
-    test1: .word 0xFFFFFFFF
-    test2: .word 0x1
-    
+    w0:	.word 0xf2345678
+    w1:	.word 0xff654321
 .text
-.globl main
+    .globl main
 
-main: 
-
-    lw $t1, test1 
-    lw $t2, test2
-
-    addu $s1, $t1, $t2
-    sltu $s2, $s1, $t1
-
-    li $v0, 10
-    syscall #exit
-
+main:
+    # load
+    lw   $s0, w0
+    lw   $s1, w1
+    addu $s2, $s1, $s0 # sum
+    # si'ai + si'bi
+    nor  $s2, $s2, $zero
+    or   $s3, $s1, $s0
+    and  $s3, $s3, $s2
+    # + aibi
+    and  $t0, $s1, $s0
+    or   $s3, $s3, $t0
+    # shift to leave 1 bit
+    srl  $s3, $s3, 31
+    # exit
+    li   $v0, 10
+    syscall
