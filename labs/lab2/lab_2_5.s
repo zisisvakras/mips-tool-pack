b180bd286072f979858521456e7ca1b7d5e97989
Zisis Vakras <zisis.vakras@protonmail.com>
2024-05-15 22:11:04 +0300

    moved 2.5 to alt

--- labs/lab2/lab_2_5.s
+++ labs/lab2/lab_2_5.s
@@ -1,23 +1,23 @@
+# Optimal solution for lab_2_5
+# Refer to alternative for xergias sol
+
 .data
-    w0:	.word 0xf2345678
-    w1:	.word 0xff654321
+    w1: .word 0xFFFFFFFF
+    w2: .word 0x1
+
 .text
-    .globl main
+.globl main
 
 main:
-    # load
-    lw   $s0, w0
-    lw   $s1, w1
-    addu $s2, $s1, $s0 # sum
-    # si'ai + si'bi
-    nor  $s2, $s2, $zero
-    or   $s3, $s1, $s0
-    and  $s3, $s3, $s2
-    # + aibi
-    and  $t0, $s1, $s0
-    or   $s3, $s3, $t0
-    # shift to leave 1 bit
-    srl  $s3, $s3, 31
-    # exit
-    li   $v0, 10
+    # Load
+    lw $t1, w1
+    lw $t2, w2
+
+    # Result - Carry < W1, W2
+    addu $s1, $t1, $t2
+    sltu $s2, $s1, $t1
+
+    # Exit
+    li $v0, 10
     syscall
+
