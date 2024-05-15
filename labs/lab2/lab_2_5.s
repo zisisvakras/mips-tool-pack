2e42e1560b5b2b8d9b4893d55056f5a33e53ec93
George Siafarikas <52412315+siafgeorge@users.noreply.github.com>
2024-05-15 21:39:54 +0300

    Updating lab2.5 with 2 instructions.

--- labs/lab2/lab_2_5.s
+++ labs/lab2/lab_2_5.s
@@ -1,20 +1,18 @@
 .data
-    w0:	.word 0xf2345678
-    w1:	.word 0xff654321
+    test1: .word 0xFFFFFFFF
+    test2: .word 0x1
+    
 .text
-    .globl main
+.globl main
 
-main:
-    # load
-    lw   $t1, w0
-    lw   $t2, w1
+main: 
 
-    addu $s0, $t1, $t2
-    xor $s1, $s0, $t1
-    xor $s2, $s0, $t2
+    lw $t1, test1 
+    lw $t2, test2
 
-    or $s3, $s1, $s2
-    srl $s3, $s3, 31
-    # exit
-    li   $v0, 10
-    syscall
+    addu $s1, $t1, $t2
+    sltu $s2, $s1, $t1
+
+    li $v0, 10
+    syscall #exit
+
